368e448babd272fea016ee59de3a27137abb2082
nguyenthanhlinh <thanhlinh12314@gmail.com>
2018-04-18 14:46:17 +0700

    class fan

---
+++ FanTest.java
@@ -0,0 +1,9 @@
+public class FanTest {
+    public static void main(String[] args) {
+        Fan fanOne = new Fan(3, true, 10, "Yellow");
+        System.out.println(fanOne);
+
+        Fan fanTwo = new Fan(2, false, 5, "Blue");
+        System.out.println(fanTwo);
+    }
+}
